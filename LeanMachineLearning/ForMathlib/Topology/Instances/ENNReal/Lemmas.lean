94c1f9ceceb6ebf63c15becf8e8c2de0f4b0c237
Gaëtan Serré <56162277+gaetanserre@users.noreply.github.com>
2026-07-30 13:54:47 +0200

    Update LeanMachineLearning/ForMathlib/Topology/Instances/ENNReal/Lemmas.lean
    Co-authored-by: Rémy Degenne <remydegenne@gmail.com>

--- LeanMachineLearning/ForMathlib/Topology/Instances/ENNReal/Lemmas.lean
+++ LeanMachineLearning/ForMathlib/Topology/Instances/ENNReal/Lemmas.lean
@@ -19,7 +19,7 @@ open scoped Topology
 
 namespace ENNReal
 
-lemma tendsto_zero_le {α : Type*} {f g : α → ℝ≥0∞} {ι : Filter α}
+lemma tendsto_zero_of_le {α : Type*} {f g : α → ℝ≥0∞} {ι : Filter α}
     (hg : Tendsto g ι (𝓝 0)) (h : f ≤ g) : Tendsto f ι (𝓝 0) := by
   refine tendsto_of_tendsto_of_tendsto_of_le_of_le (g := fun _ ↦ 0) tendsto_const_nhds hg ?_ h
   intro
